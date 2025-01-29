0549f34ca1be90f1e17ffef5d48f3ba2cdd6676a
Konrad Kügler <swamblumat-git@yahoo.de>
2025-01-30 00:26:34 +0100

    feat: Add removeListeners() method
    This allows clearing the listeners without having access to
    the same function instance(s) we had when registering.

--- index.ts
+++ index.ts
@@ -5,11 +5,12 @@ import { onMounted, onBeforeUnmount, getCurrentScope } from 'vue'
 type EventCallback<T = unknown> = [T] extends [undefined] ? () => void : (x: T) => void;
 
 type GenericSource<T> = {
-	addListener: (eventListener: EventCallback<T>) => void;
-	removeListener: (eventListener: EventCallback<T>) => void;
+	addListener(eventListener: EventCallback<T>): void;
+	removeListener(eventListener: EventCallback<T>): void;
+	removeListeners(): void;
 	emit(data: T): void;
 };
-export type EventSource<T> = undefined extends T ? GenericSource<T> & { emit(): void; } : GenericSource<T>;
+export type EventSource<T> = undefined extends T ? GenericSource<T> & { emit(): void } : GenericSource<T>;
 
 export function useExternalEvent<T>(eventRegistry: EventSource<T>, eventListener: EventCallback<T>) {
 	if (!getCurrentScope()) {
@@ -28,7 +29,7 @@ export function useExternalEvent<T>(eventRegistry: EventSource<T>, eventListener
  * Creates a registry for a single event kind.
  *
  * @returns An object, that should be passed as a prop to the child component. Has an 'emit()' property that can be
- * used to fire the event from the parent. The child should call "useExternalEvent(eventPropFromParent, myCallback)" 
+ * used to fire the event from the parent. The child should call "useExternalEvent(eventPropFromParent, myCallback)"
  * in the setup function to register an event callback.
  */
 export function newEventSource<T>(): EventSource<T> {
@@ -42,6 +43,9 @@ export function newEventSource<T>(): EventSource<T> {
 			callbacks.splice(index, 1);
 		}
 	}
+	function removeListeners() {
+		callbacks.splice(0, callbacks.length);
+	}
 
 	return {
 		emit(data: T) {
@@ -49,5 +53,6 @@ export function newEventSource<T>(): EventSource<T> {
 		},
 		addListener,
 		removeListener,
+		removeListeners,
 	} as EventSource<T>;
 }
